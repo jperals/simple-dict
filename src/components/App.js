4799f64f0fd1c7ce5cdb6777407e7b63ce70be46
Joan Perals <joan.perals@ixds.com>
2017-07-22 23:05:32 +0200

    Successfully load hardcoded dictionary

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react'
+import yaml from 'js-yaml'
+import fs from 'fs'
 
+const content = yaml.safeLoad(fs.readFileSync('src/data/laia-ca.yaml'))
 class DictionaryApp extends Component {
     constructor(props) {
         super(props)
     }
     render() {
         return (
-            <h1>
-                Hello!
-            </h1>
+            <div>
+                {
+                    Object.keys(content).map(function(key, i) {
+                        return <div class="word"><h2>{key}</h2><p>{content[key].translation}</p></div>
+                    })
+                }
+            </div>
         )
     }
 }
+
+
 
 export default DictionaryApp
