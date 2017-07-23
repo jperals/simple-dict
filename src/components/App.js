5f1618454cd2614358e826f82e8420125cc8e18b
Joan Perals <joan.perals@ixds.com>
2017-07-23 17:23:20 +0200

    Various relatively minor improvements

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import fs from 'fs'
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import yaml from 'js-yaml'
 
@@ -7,25 +7,29 @@ class DictionaryApp extends Component {
     constructor(props) {
         super(props)
     }
+
     static contextTypes = {
         store: PropTypes.object.isRequired
     }
+
     render() {
         const dictId = this.context.store.getState().dictId
         const dictPath = 'src/data/' + dictId + '.yaml'
         const content = yaml.safeLoad(fs.readFileSync(dictPath))
         return (
-            <div>
+            <ul>
                 {
-                    Object.keys(content).map(function(key, i) {
-                        return <div class="word" key={key}><h2>{key}</h2><p>{content[key].translation}</p></div>
+                    Object.keys(content).map(function (term, i) {
+                        return <li class="term" key={'term-' + i}>
+                            <h2>{term}</h2>
+                            <p>{content[term].translation}</p>
+                        </li>
                     })
                 }
-            </div>
+            </ul>
         )
     }
 }
-
 
 
 export default DictionaryApp
