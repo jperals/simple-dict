05a3cb13f91e73f08ff94c922f33ec24b3fc6b62
Joan Perals <joan.perals@ixds.com>
2017-07-24 23:20:35 +0200

    Update file path

--- count-words.js
+++ count-words.js
@@ -1,5 +1,5 @@
 const yaml = require('js-yaml');
 const fs   = require('fs');
-const dict = yaml.safeLoad(fs.readFileSync('./src/data/laia-ca.yaml'));
+const dict = yaml.safeLoad(fs.readFileSync('./data/dicts/laia-ca.yaml'));
 const ks = Object.keys(dict);
 console.log(ks.length);
