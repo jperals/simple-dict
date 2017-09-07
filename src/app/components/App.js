cc1d1a5900fa5ceb9b82ab757b99bfa02baa4d0e
Joan Perals <joan.perals@ixds.com>
2017-09-07 23:01:47 +0200

    Add dummy search box component

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import SearchBox from './SearchBox'
 import Term from './Term'
 
 class DictionaryApp extends Component {
@@ -12,17 +13,20 @@ class DictionaryApp extends Component {
         const dictId = this.context.store.getState().dictId
         const content = this.context.store.getState().dictContent
         return (
-            <ul className="terms">
-                {
-                    Object.keys(content).map(function (term, i) {
-                        return (
-                            <li key={ 'term-' + i }>
-                                <Term term={ term } termData={ content[term] } />
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+            <div>
+                <SearchBox></SearchBox>
+                <ul className="terms">
+                    {
+                        Object.keys(content).map(function (term, i) {
+                            return (
+                                <li key={ 'term-' + i }>
+                                    <Term term={ term } termData={ content[term] } />
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
         )
     }
 }
---
+++ src/app/components/SearchBox.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class SearchBox extends Component {
+    render() {
+        return (
+            <input type="text"></input>
+        )
+    }
+}
+
+export default SearchBox
