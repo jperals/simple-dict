aedba3358f0a5620bd8ce186db3ef7f7b1b66e5b
Joan Perals <joan.perals@ixds.com>
2017-09-11 18:24:26 +0200

    Styling

--- src/app/components/Term.js
+++ src/app/components/Term.js
@@ -10,15 +10,31 @@ class Term extends Component {
         return (
             <div className="term">
                 <div className="anchor" id={this.props.term}></div>
-                <span className="written-form">
-                    <a href={'#' + this.props.term}>
-                        {this.props.term}
-                    </a>
-                </span>
-                {
-                    data.pronunciation &&
-                    <span className="pronunciation">{ data.pronunciation }</span>
-                }
+                <div className="term-head">
+                    <span className="written-form">
+                        <a href={'#' + this.props.term}>
+                            {this.props.term}
+                        </a>
+                    </span>
+                    {
+                        data.pronunciation &&
+                        <span className="pronunciation">{ data.pronunciation }</span>
+                    }
+                    {
+                        data.source &&
+                        <div className="source" dangerouslySetInnerHTML={ {__html: md.renderInline(data.source)} }/>
+                    }
+                    {
+                        data.since &&
+                        <div className="since">afegit: { data.since }</div>
+                    }
+                    {
+                        data.synonyms instanceof Array && (
+                            <div className="synonyms">Sinònims: { data.synonyms.join(', ') }</div>
+                        )
+                    }
+                </div>
+                <div className="term-body">
                 {
                     data instanceof Array ? (
                         <ol className="translations">
@@ -36,19 +52,7 @@ class Term extends Component {
                         <Definition definition={data}/>
                     )
                 }
-                {
-                    data.source &&
-                    <div className="source" dangerouslySetInnerHTML={ {__html: md.renderInline(data.source)} }/>
-                }
-                {
-                    data.since &&
-                    <div className="since">afegit: { data.since }</div>
-                }
-                {
-                    data.synonyms instanceof Array && (
-                        <div className="synonyms">Sinònims: { data.synonyms.join(', ') }</div>
-                    )
-                }
+                </div>
             </div>
         )
     }
